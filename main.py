0d6a8e8d87706adc17852724401fef26b3568dae
rgibby818 <ryan.l.gibson@protonmail.com>
2024-09-06 23:15:01 -0400

    work in progress

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH,
                                       constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,8 @@ def main():
                 return
         screen.fill(000)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000
 
 
 if __name__ == "__main__":
